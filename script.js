82475484df7c38bc59b8175415e58c1c896a929c
GaoYeGithub <152664000+GaoYeGithub@users.noreply.github.com>
2024-07-09 17:55:28 +0000

    Tried adding css and js ide to it I read the documentation but it doesn't look like it supported

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
-var url = null
+let editors = {
+  html: ace.edit('editor'),
+  css: null,
+  js: null
+};
 
-const editor = ace.edit('editor')
-editor.setTheme('ace/theme/dracula')
-editor.getSession().setMode('ace/mode/html')
+let currentTab = 'html';
 
-function createUrl(html) {
-  var blob = new Blob([html], { type: 'text/html' })
-  return URL.createObjectURL(blob)
+editors.html.setTheme('ace/theme/dracula');
+editors.html.getSession().setMode('ace/mode/html');
+
+function switchTab(tab) {
+  if (!editors[tab]) {
+    editors[tab] = ace.edit('editor');
+    editors[tab].setTheme('ace/theme/dracula');
+    editors[tab].getSession().setMode(`ace/mode/${tab}`);
+  }
+
+  editors[currentTab].container.style.display = 'none';
+  editors[tab].container.style.display = 'block';
+  currentTab = tab;
+  updateTabButtons();
 }
 
-function removeUrl(url) {
-  URL.revokeObjectURL(url)
+function updateTabButtons() {
+  document.querySelectorAll('#tabs button').forEach(button => {
+    button.classList.remove('active');
+  });
+  document.querySelector(`#tabs button[onclick="switchTab('${currentTab}')"]`).classList.add('active');
 }
 
-function getIframe() {
-  var iframe = document.getElementById('iframe')
-  return iframe
+function runCode() {
+  let htmlCode = editors.html.getValue();
+  let cssCode = editors.css ? editors.css.getValue() : '';
+  let jsCode = editors.js ? editors.js.getValue() : '';
+
+  let combinedCode = `
+    <style>${cssCode}</style>
+    ${htmlCode}
+    <script>${jsCode}<\/script>
+  `;
+
+  let blob = new Blob([combinedCode], { type: 'text/html' });
+  let url = URL.createObjectURL(blob);
+  document.getElementById('iframe').src = url;
 }
 
-function setIframeUrl(url) {
-  var iframe = getIframe()
-  iframe.src = url
-}
-
-function getEditorCode() {
-  return editor.getValue()
-}
-
-function buttonclick() {
-  var code = getEditorCode()
-  removeUrl(url)
-  url = createUrl(code)
-  setIframeUrl(url)
-}
+window.onload = () => {
+  switchTab('html');
+};
